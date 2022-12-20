7d5f264af62eef3921edc80249e5bed2057c667c
Hassan <mhassandata@gmail.com>
2022-12-20 22:50:00 +0500

    update varaible.py

--- variable/variable.py
+++ variable/variable.py
@@ -12,5 +12,35 @@ print(my_favourite_color)
 
 
 #Another method of declear variables
+#A variable is created using an assignment statement. 
+# It begins with the variable's name, followed by the assignment operator = followed by the value to be stored within the variable. 
+# Note that the assignment operator = is different from the equality comparison operator ==.
+#You can also assign values to multiple variables in a single statement by separating the variable names and values with commas.
 col1, col2, col3 = "green", "red", "blue"
 print(col1, col2, col3)
+
+#You can assign the same value to multiple variables by chaining multiple assignment operations within a single statement.
+col4 = col5 = col6 = "white"
+print(col4)
+print(col5)
+
+#The pattern var = var op something (where op is an arithmetic operator like +, -, *, /) is very common, 
+# so Python provides a shorthand syntax for it.
+counter = 10
+counter+= 1
+print(counter)
+
+################################################################################################################
+'''Variable names can be short (a, x, y, etc.) or descriptive ( my_favorite_color, profit_margin, the_3_musketeers, etc.). However, you must follow these rules while naming Python variables:
+
+A variable's name must start with a letter or the underscore character _. It cannot begin with a number.
+A variable name can only contain lowercase (small) or uppercase (capital) letters, digits, or underscores (a-z, A-Z, 0-9, and _).
+Variable names are case-sensitive, i.e., a_variable, A_Variable, and A_VARIABLE are all different variables.'''
+
+#Here are some valid variable names:
+a_variable = 23
+is_today_Saturday = False
+my_favorite_car = "Delorean"
+the_3_musketeers = ["Athos", "Porthos", "Aramis"]
+
+print(the_3_musketeers)
