f8ac0f7098e3aed1269251765de2fbe060e0a996
Hassan <mhassandata@gmail.com>
2022-12-23 22:23:12 +0500

    updat my_list till negative indexing

--- list/my_list.py
+++ list/my_list.py
@@ -24,3 +24,27 @@ print(nested_list)
 
 mixed_nested_list = [1, 2, ['mango', "banana"], 5.5]
 print(mixed_nested_list)
+
+
+#Accessing specific element of list (indexing)
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[4])
+#it will display 5 as list indexing start with 0
+
+
+# Nested indexing
+my_list = ["Mitch", [3, 6, 7], ["yellow", 5, 6]]
+print(my_list)
+
+sub_list = my_list[1]
+print(sub_list)
+#Now it will display [3,6,7] the whole list is in index 1
+
+#Checking while an element is in the list or not
+print(6 in my_list)
+# it will display boolean out as for this condition it will display "False"
+
+
+#As far the list has positive indexing as well as negative indexing
+print(my_list[-1])
+#It will display last element of the list
