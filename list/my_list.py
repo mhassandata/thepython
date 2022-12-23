ca608c296cc9280685711e0c466a1a840e39d4a4
Hassan <mhassandata@gmail.com>
2022-12-23 22:04:28 +0500

    updated my_list.py

--- list/my_list.py
+++ list/my_list.py
@@ -4,3 +4,13 @@ my_list = []
 #list with integer values
 list1 = [1,2,3,4,5,]
 print(list1)
+
+#List with float values
+list_float = [1.5, 2.5, 2.9, 3.0]
+print(list_float)
+
+#list of string valuse as following
+list_string = ["python", "java", "c", "c#"]
+print(list_string)
+print(type(list_string))
+
