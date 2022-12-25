cd1802882f55b143aeb4b1332f9e0b835b13d6bc
Hassan <mhassandata@gmail.com>
2022-12-25 17:40:14 +0500

    added dictionary first time

---
+++ dictionary/my_dict.py
@@ -0,0 +1,16 @@
+#Dictionary is the data type in python which has value and key combination in a pair. which is in close in square braket.
+this_is_dict = {}
+print(type(this_is_dict))
+#this has display <class 'dict'>
+
+#let have create student dictionary which has student dictionary
+
+student  = {
+    "f_name" : "Hassan",
+    "l_name" : "Ali",
+    "roll_num" : "kiu-1099",
+    "email" : "hassan@kiu.edu.pk",
+    "semester" : "8th"
+}
+print(student)
+#here f_name is key and "Hassan" is a value which has key value pair.
