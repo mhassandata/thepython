bb0ffe9e6ca70b440815e151ba188e2af8c692ee
Hassan <mhassandata@gmail.com>
2023-01-03 14:44:16 +0500

    Update my_loop.py file

--- Iteration(loops)/my_loop.py
+++ Iteration(loops)/my_loop.py
@@ -23,3 +23,23 @@ for j in range(1, 20, 2):
     print(j)
 #ouput will be: 1 3 5 7 9 11 13 15 17 19
 
+for num in range(10, 0 -2):
+    print(num)
+#This will display digits from 10 to 0 reverse base by steping 2 down.
+
+#Checking the characters in full words
+for char in "Pakistan":
+    print(char)
+#but int object is not iterable.
+for item in ["Keyboard","Mouse","Monitor","Headfone","Pen","Notebook"]:
+    print(item.upper())
+#output: KEYBOARD MOUSE MONITOR HEADFONE PEN NOTEBOOK
+
+items = ["Bag","Ball","Cap","Copy","Knife","Keyboard","Mouse","Monitor","Headfone","Pen","Notebook"]
+
+for item in items:
+    if item.endswith("e"):
+        print(item)
+for item1 in items:
+    if item1.startswith("C"):
+        print(item1)
