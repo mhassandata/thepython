6b0de759c6a834a6c405cd419a804f386814111d
Hassan <mhassandata@gmail.com>
2023-01-03 14:30:36 +0500

    Added my_loop.py and deleted loop foder

---
+++ Iteration(loops)/my_loop.py
@@ -0,0 +1,25 @@
+#Loop in using in python for iteration think repeating in certain conditions.
+#if we need to print digit from 1 to 10
+for i in range(10):
+    print(i)
+#The output would like  0 1 2 3 4 5 6 7 8 9
+"""
+Python Loops
+For loop
+While Loop
+
+"""
+#For loop
+for i in range(10):
+    print(i, "Hello World")
+#It will print "Hello World" ten time.
+#You will give the range funciton to start and end point like
+for i in range(1, 10):
+    print(i ,"Hello world")
+#The difference between this loop and above are, this will start from 1 while above will start from zero.
+
+#Now we will introduce another factor in range functio and that is step (start, end, step)
+for j in range(1, 20, 2):
+    print(j)
+#ouput will be: 1 3 5 7 9 11 13 15 17 19
+
--- Loop/my_loop.py
+++
@@ -1,5 +0,0 @@
-#Loop in using in python for iteration think repeating in certain conditions.
-#if we need to print digit from 1 to 10
-for i in range(10):
-    print(i)
-#The output would like  0 1 2 3 4 5 6 7 8 9    
