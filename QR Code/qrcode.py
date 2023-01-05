649f60d06a3b55639539d7cb557bb65fcfdc0e91
Hassan <mhassandata@gmail.com>
2023-01-06 01:26:52 +0500

    Added qrcode

---
+++ QR Code/qrcode.py
@@ -0,0 +1,19 @@
+import qrcode
+
+# Create the QR code object
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add the YouTube channel URL to the QR code object
+qr.add_data("https://www.youtube.com/@PIAIC")
+qr.make(fit=True)
+
+# Create an image from the QR code data
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image to a file
+img.save("youtube_channel_qr_code.png")
