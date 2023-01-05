86adaa3c25727f6bd1d98bf668e89216c7a2e16e
Hassan <mhassandata@gmail.com>
2023-01-06 01:35:00 +0500

    Updated qrcode.py

--- QR Code/qrcode.py
+++ QR Code/qrcode.py
@@ -1,19 +1,27 @@
-import qrcode
+# import qrcode
 
-# Create the QR code object
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
+# # Create the QR code object
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=10,
+#     border=4,
+# )
 
-# Add the YouTube channel URL to the QR code object
-qr.add_data("https://www.youtube.com/@PIAIC")
-qr.make(fit=True)
+# # Add the YouTube channel URL to the QR code object
+# qr.add_data("https://www.youtube.com/@PIAIC")
+# qr.make(fit=True)
 
-# Create an image from the QR code data
-img = qr.make_image(fill_color="black", back_color="white")
+# # Create an image from the QR code data
+# img = qr.make_image(fill_color="black", back_color="white")
 
-# Save the QR code image to a file
-img.save("youtube_channel_qr_code.png")
+# # Save the QR code image to a file
+# img.save("youtube_channel_qr_code.png")
+
+import qrcode as qr
+
+# Create the QR code image file
+img = qr.make("https://www.youtube.com/@skdesigns268")
+
+# Save the QR code image file
+img.save("SKDESIGNS.png")
