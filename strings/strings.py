f4094aa7b82c253daec76914972afe47bc704d2d
Hassan <mhassandata@gmail.com>
2023-01-12 23:55:55 +0500

    Updated string.py file'

--- strings/strings.py
+++ strings/strings.py
@@ -7,3 +7,23 @@ String can be enclosed by either double or single quotes
 
 my_string = "Hello, Python"
 print(my_string)
+
+
+#you can concatenate the string by + sign
+first_name = "Mohd"
+second_name = "Hassan"
+full_name = first_name + second_name
+print(full_name)
+
+
+#You can split sentence by split method 
+sentence = "My name is Mohd Hassan"
+word = sentence.split()
+print(word)
+#Output: ['My', 'name', 'is', 'Mohd', 'Hassan']
+
+#You can split the email by using split method
+my_email = "mhassan@gmail.com"
+name = my_email.split('@')
+print(name)
+#Output: ['mhassan', 'gmail.com']
