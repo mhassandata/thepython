d6bcdda82abdf97dab7eab3dd27f127685bb308a
Hassan <mhassandata@gmail.com>
2023-01-17 22:15:49 +0500

    Updated the lambda.py file

--- Lambda_Functions/lambda.py
+++ Lambda_Functions/lambda.py
@@ -25,3 +25,14 @@ print(even_20)
 even_g_20 = [num for num in range(40) if num%2==0 if num >= 20]
 print(even_g_20)
 #output: [20, 22, 24, 26, 28, 30, 32, 34, 36, 38]
+
+
+#print the odd and even number from 0 to 20 with list comphrension
+
+even_odd_20 = [f'{num } is Even 'if num % 2 ==0 else f"{num} is odd" for num in range(20)]
+print(even_odd_20) 
+"""
+#output: ['0 is Even ', '1 is odd', '2 is Even ', '3 is odd', '4 is Even ', '5 is odd', 
+# '6 is Even ', '7 is odd', '8 is Even ', '9 is odd', '10 is Even ', '11 is odd', 
+# '12 is Even ', '13 is odd', '14 is Even ', '15 is odd', '16 is Even ', '17 is odd', '18 is Even ', '19 is odd']
+"""
