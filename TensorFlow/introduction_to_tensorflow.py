afdf5c0584f51359960df7be4d1068979acdd6ca
Hassan <mhassandata@gmail.com>
2023-07-29 17:28:27 +0500

    Updated tensorflwo till exercise 1

--- TensorFlow/introduction_to_tensorflow.py
+++ TensorFlow/introduction_to_tensorflow.py
@@ -70,3 +70,31 @@ b = tf.constant(2, tf.float32)
 # Compute their product
 c0 = tf.multiply(a0, b)
 c1 = a0*b
+
+
+#Exerise 
+# Import constant from TensorFlow
+from tensorflow import constant
+
+# Convert the credit_numpy array into a tensorflow constant
+credit_constant = constant(credit_numpy)
+
+# Print constant datatype
+print('\n The datatype is:', credit_constant.dtype)
+
+# Print constant shape
+print('\n The shape is:', credit_constant.shape)
+
+
+#2
+# Define the 1-dimensional variable A1
+A1 = Variable([1, 2, 3, 4])
+
+# Print the variable A1
+print('\n A1: ', A1)
+
+# Convert A1 to a numpy array and assign it to B1
+B1 = A1.numpy()
+
+# Print B1
+print('\n B1: ', B1)
