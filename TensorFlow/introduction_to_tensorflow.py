376d20a5a9444f3df9d246ea24f0dcc823bb5180
Hassan <mhassandata@gmail.com>
2023-07-29 16:49:38 +0500

    Added tensorflw file

---
+++ TensorFlow/introduction_to_tensorflow.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+# 0D Tensor
+d0 = tf.ones((1,))
+print(d0)
+
+# 1D Tensor
+d1 = tf.ones((2,))
+print(d1)
+
+# 2D Tensor
+d2 = tf.ones((2,2,))
+print(d2)
+
+# 3D Tensor
+d3 = tf.ones((2,2,2))
+print(d3)
