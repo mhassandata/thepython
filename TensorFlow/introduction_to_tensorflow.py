73d6eb95dc97e1495d6abf4f0de86eb3013455c3
Hassan <mhassandata@gmail.com>
2023-07-29 17:09:23 +0500

    Updated the tensorflow file

--- TensorFlow/introduction_to_tensorflow.py
+++ TensorFlow/introduction_to_tensorflow.py
@@ -42,3 +42,31 @@ print(d3.numpy())
  [[1. 1.]
   [1. 1.]]]
 '''
+
+
+#Defining constants in Tensorflow
+'''
+A constant is the simplest category of tensor
+    - Not trainable
+    - Can have any dimension
+'''
+from tensorflow import constant
+#Define a 2X3 constant.
+a = constant(3, shape=[2,3])
+#Define a 2X2 constant
+b = constant([1,2,3,4], shape=[2,2])
+
+
+#Defining and initialization variable
+import tensorflow as tf
+
+#Define a variable
+a0 = tf.Variable([1,2,3,4,5,6], dtype=tf.float32)
+a1 = tf.Variable([1,2,3,4,5,6], dtype=tf.float16)
+
+#Define a constant
+b = tf.constant(2, tf.float32)
+
+# Compute their product
+c0 = tf.multiply(a0, b)
+c1 = a0*b
