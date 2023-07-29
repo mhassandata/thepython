1a5c06585e281ee942955fce4ce9c202461d193d
Hassan <mhassandata@gmail.com>
2023-07-29 16:58:28 +0500

    Updated the tensorflow file

--- TensorFlow/introduction_to_tensorflow.py
+++ TensorFlow/introduction_to_tensorflow.py
@@ -1,5 +1,19 @@
+# What is TensorFlow
+"""
+Open-source library for graph-based numerical computation
+    1. Developed by the Google Brain Team
+Low and high level APIs
+    1. Addition, multiplication, differentiation
+    2. Machine learning models.
+"""
 import tensorflow as tf
 
+'''
+What is a tensor?
+1. Generalization of vectors and matrices
+2. Collection of numbers
+3. Specific shape
+'''
 # 0D Tensor
 d0 = tf.ones((1,))
 print(d0)
@@ -15,3 +29,16 @@ print(d2)
 # 3D Tensor
 d3 = tf.ones((2,2,2))
 print(d3)
+'''
+[[1. 1.]
+  [1. 1.]]], shape=(2, 2, 2), dtype=float32)
+'''
+# Print the 3D tensor
+print(d3.numpy())
+'''
+[[[1. 1.]
+  [1. 1.]]
+
+ [[1. 1.]
+  [1. 1.]]]
+'''
